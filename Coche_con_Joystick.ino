d31706203d2aabaf590dd91e6ad52e0eaed2da12
Jonathan Lorca <91257407+LorcaJonathan@users.noreply.github.com>
2023-07-09 19:42:26 +0200

    Update Coche_con_Joystick.ino

--- Coche_con_Joystick.ino
+++ Coche_con_Joystick.ino
@@ -1,11 +1,11 @@
 // Definición de los pines de entrada y salida
-int speedPin1 = 3;     // Pin de velocidad del motor 1
-int dirPin1 = 4;       // Pin de dirección 1 del motor 1
-int dirPin2 = 5;       // Pin de dirección 2 del motor 1
+int speedPin1 = 11;     // Pin de velocidad del motor 1
+int dirPin1 = 8;       // Pin de dirección 1 del motor 1
+int dirPin2 = 9;       // Pin de dirección 2 del motor 1
 
-int speedPin2 = 6;     // Pin de velocidad del motor 2
-int dirPin3 = 7;       // Pin de dirección 1 del motor 2
-int dirPin4 = 8;       // Pin de dirección 2 del motor 2
+int speedPin2 = 10;     // Pin de velocidad del motor 2
+int dirPin3 = 6;       // Pin de dirección 1 del motor 2
+int dirPin4 = 7;       // Pin de dirección 2 del motor 2
 
 int yVal = A0;         // Pin de lectura analógica
 int readValue;         // Variable para almacenar el valor leído del joystick del eje Y
