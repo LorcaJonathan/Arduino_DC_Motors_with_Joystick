4bdb519deed70e1154025b2a06f1128cf49f4c64
Jonathan Lorca <91257407+LorcaJonathan@users.noreply.github.com>
2023-07-09 19:29:27 +0200

    Update Coche_con_Joystick.ino

--- Coche_con_Joystick.ino
+++ Coche_con_Joystick.ino
@@ -1,14 +1,14 @@
 // Definición de los pines de entrada y salida
-int speedPin1 = 3;   // Pin de velocidad del motor 1
-int dirPin1 = 4;     // Pin de dirección 1 del motor 1
-int dirPin2 = 5;     // Pin de dirección 2 del motor 1
+int speedPin1 = 3;     // Pin de velocidad del motor 1
+int dirPin1 = 4;       // Pin de dirección 1 del motor 1
+int dirPin2 = 5;       // Pin de dirección 2 del motor 1
 
-int speedPin2 = 6;   // Pin de velocidad del motor 2
-int dirPin3 = 7;     // Pin de dirección 1 del motor 2
-int dirPin4 = 8;     // Pin de dirección 2 del motor 2
+int speedPin2 = 6;     // Pin de velocidad del motor 2
+int dirPin3 = 7;       // Pin de dirección 1 del motor 2
+int dirPin4 = 8;       // Pin de dirección 2 del motor 2
 
-int yVal = A0;    // Pin de lectura analógica
-int readValue;       // Variable para almacenar el valor leído del joystick del eje Y
+int yVal = A0;         // Pin de lectura analógica
+int readValue;         // Variable para almacenar el valor leído del joystick del eje Y
 
 void setup() {
   // Configuración de los pines como entrada o salida
@@ -32,25 +32,25 @@ void loop() {
   analogWrite(speedPin2, 255);
 
   // Comprobar el valor leído para determinar la dirección del motor 1
-  if (readValue < 400) {  // Si el valor del joystick es menor de 400 del eje Y
-    digitalWrite(dirPin1, HIGH);   // Establecer dirección 1 del motor 1 como HIGH
-    digitalWrite(dirPin2, LOW);    // Establecer dirección 2 del motor 1 como LOW
+  if (readValue < 400) {    // Si el valor del joystick es menor a 400 del eje Y
+    digitalWrite(dirPin1, HIGH);     // Establecer dirección 1 del motor 1 como HIGH
+    digitalWrite(dirPin2, LOW);      // Establecer dirección 2 del motor 1 como LOW
 
-    digitalWrite(dirPin3, HIGH);
-    digitalWrite(dirPin4, LOW);
+    digitalWrite(dirPin3, HIGH);     // Establecer dirección 1 del motor 2 como HIGH
+    digitalWrite(dirPin4, LOW);      // Establecer dirección 2 del motor 2 como LOW
   }
   else if (readValue < 600) {
-    digitalWrite(dirPin1, LOW);    // Establecer dirección 1 del motor 1 como LOW
-    digitalWrite(dirPin2, LOW);    // Establecer dirección 2 del motor 1 como LOW
+    digitalWrite(dirPin1, LOW);      // Establecer dirección 1 del motor 1 como LOW
+    digitalWrite(dirPin2, LOW);      // Establecer dirección 2 del motor 1 como LOW
     
-    digitalWrite(dirPin3, LOW);
-    digitalWrite(dirPin4, LOW);
+    digitalWrite(dirPin3, LOW);      // Establecer dirección 1 del motor 2 como LOW
+    digitalWrite(dirPin4, LOW);      // Establecer dirección 2 del motor 2 como LOW
   }
   else {
-    digitalWrite(dirPin1, LOW);    // Establecer dirección 1 del motor 1 como LOW
-    digitalWrite(dirPin2, HIGH);   // Establecer dirección 2 del motor 1 como HIGH
+    digitalWrite(dirPin1, LOW);      // Establecer dirección 1 del motor 1 como LOW
+    digitalWrite(dirPin2, HIGH);     // Establecer dirección 2 del motor 1 como HIGH
 
-    digitalWrite(dirPin3, LOW);
-    digitalWrite(dirPin4, HIGH);
+    digitalWrite(dirPin3, LOW);      // Establecer dirección 1 del motor 2 como LOW
+    digitalWrite(dirPin4, HIGH);     // Establecer dirección 2 del motor 2 como HIGH
   }
 }
